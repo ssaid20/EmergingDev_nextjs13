e4e58ff55092524cf0153a9020f5e7fd328e13bb
Said Said <ssaid20@icloud.com>
2023-12-12 16:08:33 -0600

    created the structure of the navbar components and installed shadcn ui

--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -1,19 +1,14 @@
-// Importing necessary components and libraries
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
-// Navbar component
 const Navbar = () => {
   return (
-    <nav
-      // Adding styles to the navbar
-      className="flex-between background-light900_dark200 
-    fixed z-50 w-full gap-5 p-6 shadow-light-300 
-    dark:shadow-none sm:px-12"
-    >
-      {/* Logo and site name section */}
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -32,32 +27,29 @@ const Navbar = () => {
         </p>
       </Link>
 
-      {/* Placeholder for a global search component */}
-      GlobalSearch
-      
-      <div className="flex-between gap-5">
-        {/* Placeholder for a theme switcher or icon */}
-        Theme
+      <GlobalSearch />
 
-        {/* User button that displays when a user is signed in */}
+      <div className="flex-between gap-5">
+        <Theme />
+
         <SignedIn>
-          <UserButton 
-            afterSignOutUrl="/" 
+          <UserButton
+            afterSignOutUrl="/"
             appearance={{
               elements: {
-                  avatarBox: 'h-10 w-10'
+                avatarBox: "h-10 w-10",
               },
               variables: {
-                  colorPrimary: '#7300ff'
-              }
+                colorPrimary: "#ff7000",
+              },
             }}
           />
         </SignedIn>
+
+        <MobileNav />
       </div>
     </nav>
   );
 };
 
-// Exporting the Navbar component
 export default Navbar;
-
