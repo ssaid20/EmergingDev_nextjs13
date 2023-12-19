48e7b29bfb8e23c30b73362ec48f607de8198a12
Said Said <ssaid20@icloud.com>
2023-12-19 12:07:48 -0600

    Updated the 'RightSidebar' component to include not only a list of top questions but also a section for popular tags.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '@/components/shared/Navbar/Navbar'
 import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
 const Layout = ({children }: {children: React.ReactNode}) => {
   return (
     <main className="background-light850_dark100 relative">
@@ -15,7 +16,7 @@ const Layout = ({children }: {children: React.ReactNode}) => {
 
             </section>
 
-            RightSidebar
+            <RightSidebar />
         </div>
         Toaster
     </main>
